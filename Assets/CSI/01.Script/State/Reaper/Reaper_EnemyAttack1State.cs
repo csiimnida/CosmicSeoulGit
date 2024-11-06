e1750e2b507095bdb90ec7f01ea37031d6d9fe1f
CSI <jjsjuseo@gmail.com>
2024-11-06 23:23:21 +0900

    Make : Reaper_Enemy

---
+++ Assets/CSI/01.Script/State/Reaper/Reaper_EnemyAttack1State.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reaper_EnemyAttack1State : EnermyState
+{
+    public Reaper_EnemyAttack1State(Enemy enemy) : base(enemy)
+    {
+    }
+    
+    protected override void EnterState()
+    {
+        _emermy.RbCompo.velocity = Vector2.zero;
+        _emermy.AnimCompo.PlayAnimaiton(AnimationType.Attack1);
+    }
+
+    public override void UpdateState()
+    {
+        _emermy.transform.localRotation = Quaternion.AngleAxis(_emermy.transform.position.x > _emermy.player.transform.position.x ? 180 : 0,Vector3.up);
+        if (_emermy.AnimCompo.Animator.GetCurrentAnimatorStateInfo(0).IsName("Attack1")
+            && _emermy.AnimCompo.Animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f)
+        {
+            Debug.Log("공격 다함");
+        }
+        if (!Physics2D.OverlapCircle(_emermy.transform.position, _emermy.DataSo.Attack_range,LayerMask.GetMask("Player")))
+        {
+            if (_emermy.Combit)
+            {
+                if (_emermy.player.GetComponent<Player>().GetHP() <= 0)
+                {
+                    _emermy.TransitionState(EnemyStateType.Idle);
+                    return;
+                }
+            }
+            _emermy.TransitionState(EnemyStateType.Move);
+        }
+    }
+
+    protected override void ExtiState()
+    {
+        
+    }
+}
---
+++ Assets/CSI/01.Script/State/Reaper/Reaper_EnemyTeleportState.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reaper_EnemyTeleportState : EnermyState
+{
+    public Reaper_EnemyTeleportState(Enemy enemy) : base(enemy)
+    {
+    }
+
+}

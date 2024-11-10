23a88a7fe289d37b183e0e3f58720eef2f5c8985
Songstrio <songstrio20080131@gmail.com>
2024-11-10 23:30:10 +0900

    SoundManager90percentClear

---
+++ Assets/JH/Scripts/So/JHSoundSO.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SO/Sounds")]
+public class JHSoundSO : ScriptableObject
+{
+    public SoundSO[] SoundSOs;
+}

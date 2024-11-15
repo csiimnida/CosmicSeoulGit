b69e4ca12d4779d5110650a71719eedc2b9de970
unknown <jjsjuseo@gmail.com>
2024-11-16 01:30:27 +0900

    Make : Boss_Reaper

---
+++ Assets/CSI/01.Script/State/Boos_Reaper/Boss_Reaper_Enemy_Attack1State.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_Reaper_Enemy_Attack1State : EnermyState
+{
+    public Boss_Reaper_Enemy_Attack1State(Enemy enemy) : base(enemy)
+    {
+    }
+
+    protected override void EnterState()
+    {
+        _emermy.RbCompo.velocity = Vector2.zero;
+        _emermy.AnimCompo.PlayAnimaiton(AnimationType.Attack1);
+        _emermy.transform.localRotation = Quaternion.AngleAxis(_emermy.transform.position.x > _emermy.player.transform.position.x ? 180 : 0,Vector3.up);
+    }
+
+    public override void UpdateState()
+    {
+        if (_emermy.AnimCompo.Animator.GetCurrentAnimatorStateInfo(0).IsName("Attack1") &&
+            _emermy.AnimCompo.Animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f)
+        {
+            _emermy.nextState = EnemyStateType.Attack1;
+            _emermy.CoolDowning = true;
+            _emermy.CoolTimeNowTimer = 0;
+            _emermy.TransitionState(EnemyStateType.Idle);
+            return;
+        }
+        if (!Physics2D.OverlapCircle(_emermy.transform.position, _emermy.DataSo.Attack_range,LayerMask.GetMask("Player")) 
+            && _emermy.AnimCompo.Animator.GetCurrentAnimatorStateInfo(0).IsName("Attack1") 
+            && _emermy.AnimCompo.Animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f)
+        {
+            if (_emermy.Combit)
+            {
+                if (_emermy.player.GetComponent<Player>().GetHP() <= 0)
+                {
+                    _emermy.TransitionState(EnemyStateType.Idle);
+                    return;
+                }
+            }
+            _emermy.TransitionState(EnemyStateType.Spawn);
+        }
+        else if (Physics2D.OverlapCircle(_emermy.transform.position, _emermy.DataSo.Attack_range,LayerMask.GetMask("Player")) 
+                 && _emermy.AnimCompo.Animator.GetCurrentAnimatorStateInfo(0).IsName("Attack1") 
+                 && _emermy.AnimCompo.Animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f)
+        {
+            _emermy.TransitionState(EnemyStateType.Attack1);
+        }
+    }
+}
---
+++ Assets/CSI/01.Script/State/Boos_Reaper/Boss_Reaper_Enemy_DeadState.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_Reaper_Enemy_DeadState : EnermyState
+{
+    public Boss_Reaper_Enemy_DeadState(Enemy enemy) : base(enemy)
+    {
+    }
+    protected override void EnterState(){
+        _emermy.AnimCompo.PlayAnimaiton(AnimationType.Death);
+        _emermy.ColCompo.enabled = false;
+        _emermy.RbCompo.bodyType = RigidbodyType2D.Static;
+    }
+}

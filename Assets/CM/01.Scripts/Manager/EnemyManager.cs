f48882a503ba1eebceef48ec5495ca0218724aec
SUSOT <a01076754042@gmail.com>
2024-11-20 00:40:41 +0900

    AddAllPlayerSound

--- Assets/CM/01.Scripts/Manager/EnemyManager.cs
+++ Assets/CM/01.Scripts/Manager/EnemyManager.cs
@@ -3,12 +3,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyManager : MonoSingleton<EnemyManager>{
+public class EnemyManager : MonoBehaviour{
+    
+    public static EnemyManager Instance;
+    
     [SerializeField] private Transform BloodBoss1Page;
     [SerializeField] private GameObject BloodBoss2PagePrefab;
     [SerializeField] GameObject BloodEffect;
     [SerializeField] private float waitTime = 3f;
-    
+
+    private void Awake(){
+        if(Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
 
     public void Start2Page(){
         StartCoroutine(CreateBloodBoss2Page());

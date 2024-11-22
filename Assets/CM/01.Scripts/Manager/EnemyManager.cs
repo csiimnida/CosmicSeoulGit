7eabc18e5f9e903858b6b73efd2927996c23080e
SUSOT <a01076754042@gmail.com>
2024-11-23 00:35:39 +0900

    AddSecondBossChangePageEffect

--- Assets/CM/01.Scripts/Manager/EnemyManager.cs
+++ Assets/CM/01.Scripts/Manager/EnemyManager.cs
@@ -9,7 +9,10 @@ public class EnemyManager : MonoBehaviour{
     
     [SerializeField] private Transform BloodBoss1Page;
     [SerializeField] private GameObject BloodBoss2PagePrefab;
-    [SerializeField] GameObject BloodEffect;
+    [SerializeField] ParticleSystem BloodEffect;
+    [SerializeField] ParticleSystem BloodEffect1;
+    [SerializeField] ParticleSystem BloodEffect2;
+    [SerializeField] ParticleSystem BloodEffect3;
     [SerializeField] private float waitTime = 3f;
 
     private void Awake(){
@@ -27,10 +30,17 @@ public class EnemyManager : MonoBehaviour{
 
 
     private IEnumerator CreateBloodBoss2Page(){
-        Instantiate(BloodEffect, BloodBoss1Page.position, Quaternion.identity);
-        yield return new WaitForSeconds(waitTime);
+        BloodEffect1.transform.position = new Vector2(BloodBoss1Page.position.x - 2f, BloodBoss1Page.position.y - 1.5f);
+        BloodEffect2.transform.position = new Vector2(BloodBoss1Page.position.x, BloodBoss1Page.position.y - 1.5f);
+        BloodEffect3.transform.position = new Vector2(BloodBoss1Page.position.x + 2f, BloodBoss1Page.position.y - 1.5f);
+        BloodEffect.transform.position = new Vector2(BloodBoss1Page.position.x, BloodBoss1Page.position.y -1.5f);
+        BloodEffect1.Play();
+        BloodEffect2.Play();
+        BloodEffect3.Play();
+        yield return new WaitForSeconds(2f);
+        BloodEffect.Play();
+        yield return new WaitForSeconds(2f);
         BloodBoss2PagePrefab.transform.position = new Vector2(BloodBoss1Page.position.x, BloodBoss1Page.position.y - 0.9f);
         Destroy(BloodBoss1Page.gameObject);
-        Destroy(BloodEffect);
     }
 }

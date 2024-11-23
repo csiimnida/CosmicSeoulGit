0afb8e5189144140d3ed2d4b0676bdccb843ecca
csiimnika <jjsjuseo@gmail.com>
2024-11-23 16:07:31 +0900

    Fatch : Ball_Flip_System

--- Assets/CSI/01.Script/Monster/Ghost_Ball/CloneGhost_BassGenerer.cs
+++ Assets/CSI/01.Script/Monster/Ghost_Ball/CloneGhost_BassGenerer.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class CloneGhost_BassGenerer : MonoBehaviour
 {
     public Transform target;
+    private Soul _soul;
 
+    private void Start(){
+        _soul = GetComponentInParent<Soul>();
+    }
     public void SpawnGhostBall()
     {
         Ghost_Ball eye = PoolManager.Instance.Pop("Ghost_Ball") as Ghost_Ball;
         eye.transform.rotation = target.parent.rotation;
         eye.transform.position = target.position;
+        eye._isSeeRight = _soul._isSeeRight;
         eye.enermyData = target.parent.GetComponent<Enemy>().DataSo;
         eye.Rotate();
     }

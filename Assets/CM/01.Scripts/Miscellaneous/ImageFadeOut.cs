f555609c390bc54b154a549cd8384104351bcc3b
SUSOT <a01076754042@gmail.com>
2024-11-25 00:49:14 +0900

    SoundAdjustment

--- Assets/CM/01.Scripts/Miscellaneous/ImageFadeOut.cs
+++ Assets/CM/01.Scripts/Miscellaneous/ImageFadeOut.cs
@@ -8,9 +8,12 @@ using UnityEngine.UI;
 public class ImageFadeOut : MonoBehaviour
 {
     [SerializeField] private float _fadeIntTime = 3f;
+    private AudioSource _backgroundBgm;
     private Image _blackImage;
 
     private void Awake(){
+        MoveNextStage nextStage = GetComponentInParent<MoveNextStage>();
+        _backgroundBgm = nextStage.BackgroundBgm;
         _blackImage = GetComponent<Image>();
         _blackImage.color = new Color(0f, 0f, 0f, 2f);
     }

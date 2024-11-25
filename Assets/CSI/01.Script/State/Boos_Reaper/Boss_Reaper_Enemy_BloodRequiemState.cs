d00747a4a33cbb9ff445744656d2917c37864891
csi <jjsjuseo@gmail.com>
2024-11-25 14:59:56 +0900

    Feet : Boss_Reaper_BloodRequiem

---
+++ Assets/CSI/01.Script/State/Boos_Reaper/Boss_Reaper_Enemy_BloodRequiemState.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_Reaper_Enemy_BloodRequiemState : EnermyState
+{
+    public Boss_Reaper_Enemy_BloodRequiemState(Enemy enemy) : base(enemy)
+    {
+    }
+
+    protected override void EnterState()
+    {
+        _emermy.AnimCompo.PlayAnimaiton(AnimationType.BloodRequiem);
+        _emermy.BloodBloodRequiemEffect.gameObject.SetActive(true);
+        _emermy.SpawnAnimator.enabled = false;
+        
+
+    }
+
+    public override void UpdateState()
+    {
+        _emermy.RbCompo.bodyType = RigidbodyType2D.Static;
+        if (_emermy.AnimCompo.Animator.GetCurrentAnimatorStateInfo(0).IsName("BloodRequiem") &&
+            _emermy.AnimCompo.Animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 3f)
+        {
+            _emermy.TransitionState(EnemyStateType.Idle);
+        }
+    }
+
+    protected override void ExtiState()
+    {
+        _emermy.SpawnAnimator.enabled = true;        
+        _emermy.BloodBloodRequiemEffect.gameObject.SetActive(false);
+
+    }
+}

a5ae5a71d5143ca5116c081b38c6c0b18c61747c
SUSOT <a01076754042@gmail.com>
2024-11-25 18:16:39 +0900

    Fetch : GameManager

--- Assets/CM/01.Scripts/Manager/GameManager.cs
+++ Assets/CM/01.Scripts/Manager/GameManager.cs
@@ -1,26 +1,39 @@
 using System;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GameManager : MonoSingleton<GameManager>
-{
-    
+public class GameManager : MonoSingleton<GameManager>{
     public Player Player;
-    public void ResetPlayer()
-    {
-        
+    private GameObject _playerPostionSetting;
+    public Transform playerPrefab;
+
+    public void ResetPlayer(){
     }
 
-    private void Awake()
-    {
-        //if(GameObject.find)
-        Player = GameObject.Find("Player").GetComponent<Player>();
-        print("AWAKE");
+    private void Start(){
+        SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
-    private void OnEnable()
-    {
-        print("이이이");
+    private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1){
+        if (arg0.name == "Tutorial") return;
+        _playerPostionSetting = GameObject.Find("PlayerSpawnPostion"); 
+
+        Transform a;
+        a = Instantiate(playerPrefab, _playerPostionSetting.transform.position, Quaternion.identity);
+        a.parent = null;
+        Player = a.GetComponent<Player>();
     }
 
+    private void Awake(){
+        if (SceneManager.GetActiveScene().name == "Tutorial") return;
+        try
+        {
+            _playerPostionSetting = GameObject.Find("PlayerSpawnPostion").GetComponent<GameObject>(); 
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+        }
+        Player = Instantiate(playerPrefab, _playerPostionSetting.transform.position, Quaternion.identity).GetComponent<Player>();
+    }
 }
--- Assets/CM/01.Scripts/State/WoodGuardian/WoodGuardian.cs
+++ Assets/CM/01.Scripts/State/WoodGuardian/WoodGuardian.cs
@@ -30,7 +30,6 @@ public class WoodGuardian : Enemy{
             }
             catch (Exception e)
             {
-                Debug.Log($"{stateType.ToString()}를 찾을수 없습니다");
             }
         }
         TransitionState(EnemyStateType.Idle);

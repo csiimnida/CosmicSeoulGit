d7cdfa2eeecefec9c7c4ccc26cac7875a6a29641
SUSOT <a01076754042@gmail.com>
2024-11-26 14:15:36 +0900

    Edit : Ending Scene

--- Assets/CM/01.Scripts/Miscellaneous/MoveBackground.cs
+++ Assets/CM/01.Scripts/Miscellaneous/MoveBackground.cs
@@ -1,12 +1,23 @@
 using System;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MoveBackground : MonoBehaviour
 {
     [SerializeField] private Transform _targetPosition;
     [SerializeField] private float _moveTime = 1f;
+    public UnityEvent OnEnd;
+    private float _curTime;
     private void Start(){
         gameObject.transform.DOMoveX(_targetPosition.position.x, _moveTime).SetEase(Ease.Linear);
+    }
+
+    private void Update(){
+        _curTime += Time.deltaTime;
+        if (_curTime >= _moveTime - 10f)
+        {
+            OnEnd?.Invoke();
+        }
     }
 }

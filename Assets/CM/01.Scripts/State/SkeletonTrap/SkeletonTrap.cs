8a14f2c439ba5e6ba4c1edb2c2b4aee48a1a3648
SUSOT <a01076754042@gmail.com>
2024-11-26 09:01:44 +0900

    Delete : Enemy Combit System

--- Assets/CM/01.Scripts/State/SkeletonTrap/SkeletonTrap.cs
+++ Assets/CM/01.Scripts/State/SkeletonTrap/SkeletonTrap.cs
@@ -44,14 +44,6 @@ public class SkeletonTrap : Enemy
     protected override void Damage_call(float damage){
         NowHp -= damage;
         StartCoroutine(Do_Hit_Effect());
-        if (!Combit)
-        {
-            Combit = true;
-            if(currentState == EnemyStateType.Attack1)
-                TransitionState(EnemyStateType.Attack1);
-            else
-                TransitionState(EnemyStateType.Move);
-        }
         if (NowHp <= 0)
         {
             print("죽음");

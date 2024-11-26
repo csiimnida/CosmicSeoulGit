349bf14fb52ab4b046ec97e8cc11ed2f5eaca05e
SUSOT <a01076754042@gmail.com>
2024-11-26 19:02:52 +0900

    Create : Boss Round Change Sound

---
+++ Assets/CM/01.Scripts/Miscellaneous/BossRoundSound.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BossRoundSound : MonoBehaviour{
+    [SerializeField] private AudioSource _backgroundBGM;
+    [SerializeField] private AudioSource _bossBGM;
+    [SerializeField] private float _fadeDuration = 3f;
+
+    private void OnEnable(){
+        StartCoroutine(BossSoundUp());
+    }
+    
+    private IEnumerator BossSoundUp(){
+        _bossBGM.gameObject.SetActive(true);
+        for (int i = 0; i < 100; i++)
+        {
+            _backgroundBGM.volume -= 0.01f;
+            _bossBGM.volume += 0.01f;
+            yield return new WaitForSeconds(0.02f);
+        }
+        yield return new WaitForSeconds(_fadeDuration + 0.5f);
+    }
+}
---
+++ Assets/CM/01.Scripts/Miscellaneous/ClearBossSound.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearBossSound : MonoBehaviour
+{
+    [SerializeField] private AudioSource _backgroundBGM;
+    [SerializeField] private AudioSource _bossBGM;
+    [SerializeField] private float _fadeDuration = 3f;
+
+    public void ClearBoss(){
+        StartCoroutine(BossSoundUp());
+    }
+    
+    private IEnumerator BossSoundUp(){
+        for (int i = 0; i < 100; i++)
+        {
+            _backgroundBGM.volume += 0.01f;
+            _bossBGM.volume -= 0.01f;
+            yield return new WaitForSeconds(0.02f);
+        }
+        yield return new WaitForSeconds(_fadeDuration + 0.5f);
+    }
+}

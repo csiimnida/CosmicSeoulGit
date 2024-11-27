b179fdf3f5b2c7223b2e9f78924c6e1fbd95d884
csi <jjsjuseo@gmail.com>
2024-11-27 20:29:32 +0900

    Fetch : CardMAinSystem

--- Assets/DY/Managers/CardCheacker.cs
+++ Assets/DY/Managers/CardCheacker.cs
@@ -1,31 +1,50 @@
 using DG.Tweening;
-using System;
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.Rendering;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class CardData
+{
+    public string Name;
+    public float Attack;
+    public float speed;
+    public float AttackSpeed;
+    public float Health;
+    public float Text;
+}
 public class CardCheacker : MonoBehaviour , IPointerEnterHandler, IPointerDownHandler , IPointerExitHandler
 {
     private int  count = 0;
-    private bool hasPointerEntered = false; // ï¿½Ã·ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½
+    private bool hasPointerEntered = false; // ?¡À??? ????
     private bool hasPointer = false;
     private Vector2 currentSize = Vector2.zero;
     private RectTransform rect = null;
     private Tween scaleTween = null;
-    public Action _OnClick;
     private bool isClick = false;
+    [SerializeField] private Sprite frontimage;
+    private CardData _data;
+    private TextMeshProUGUI _tmp;
     private bool isFront;
+    
+    
+
+
     private void Awake()
-    {rect =transform.GetChild(0).GetComponent<RectTransform>();
+    {
+        rect =transform.GetChild(0).GetComponent<RectTransform>();
+        _tmp = transform.GetChild(1).GetComponent<TextMeshProUGUI>();
         currentSize = rect.localScale;
     }
     public void OnPointerEnter(PointerEventData eventData)
     {
         if (!hasPointer)
-        { hasPointer = true;
-          scaleTween?.Kill();
-            CardManager.Instance.GameobjectGet(transform.GetChild(1).gameObject, true);
+        { 
+            hasPointer = true;
+            scaleTween?.Kill();
+            _tmp.gameObject.SetActive(true);//Text
         }
 
     }
@@ -33,27 +52,106 @@ public class CardCheacker : MonoBehaviour , IPointerEnterHandler, IPointerDownHa
      public void OnPointerExit(PointerEventData eventData)
     {
         if (hasPointer)
-        { hasPointer = false;
+        { 
+            hasPointer = false;
             scaleTween?.Kill();
-            CardManager.Instance.GameobjectGet(transform.GetChild(1).gameObject, false);
+            _tmp.gameObject.SetActive(false);//Text
+            _tmp.text = "";
         }
     }
     public void OnPointerDown(PointerEventData eventData)
     {   
-        
-     
-     
-        CardManager.Instance.FrontSprite(this.gameObject.GetComponent<CardCheacker>());
-        _OnClick?.Invoke();
-            
+        ChildChange(true,transform.GetChild(0));
         if (isFront)
         { 
+            CardManager.Instance.Enter(_data);
             CardManager.Instance.EndCardPolling();
-            CardManager.Instance.Enter(gameObject.GetComponent<CardCheacker>());
             isFront = false;
            
-        }// ï¿½Ã·ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½
+        }
         isFront = true;
         
+    }
+    private void ChildChange(bool flag,Transform child)
+    {
+        child.DOScaleX(0f, 0.2f).SetUpdate(true).OnComplete(() =>
+        {
+            RandomSprite();
+            child.DOScaleX(-2f, 0.2f).SetUpdate(true);  
+            child.GetChild(0).gameObject.SetActive(flag);
+        });
+    }
+    public void RandomSprite()
+    {
+        int stage = CardManager.Instance.stage;
+        if (stage == 0)
+            Stage0();
+        if (stage == 1)
+            Stage1();
+        if (stage == 2)
+            Stage2();
+    }
+    private void Stage2()
+    { 
+        int rand = Random.Range(0, 101);
+        if (rand <= 45)
+            EnterChange("Common");
+        if (rand > 45 && rand < 85)
+            EnterChange("Rare");
+        if (rand >= 85)
+            EnterChange("Legend");
+    }
+    private void Stage1()
+    {
+        int rand = Random.Range(0, 101);
+        if (rand <= 65)
+            EnterChange("Common");
+        if (rand is > 65 and < 90)
+            EnterChange("Rare");
+        if ( rand >= 90)
+            EnterChange("Legend");    
+    }
+    public void Stage0()
+    {int rand = Random.Range(0, 101);
+        if (rand <= 80)
+            EnterChange("Common");
+        
+        if (rand > 80)
+            EnterChange("Rare");
+    }
+    
+    private void EnterChange(string t)
+    {
+        AwakeSO[] _randomCardSO = Resources.LoadAll<AwakeSO>($"RandomSprite/{t}");
+        int spriterand = Random.Range(0, _randomCardSO.Length);
+        AwakeSO card = _randomCardSO[spriterand];
+        
+        Transform childs = transform.GetChild(0).GetChild(0);
+        
+        childs.GetComponent<Image>().sprite = card.sprite;
+        transform.GetChild(0).GetComponent<Image>().sprite = frontimage;
+        childs.GetChild(0).GetComponent<TextMeshProUGUI>().text = card.Name;
+        childs.GetChild(1).GetComponent<TextMeshProUGUI>().text = "°ø°Ý·Â:" + card.Attack  +"%";
+        childs.GetChild(2).GetComponent<TextMeshProUGUI>().text = "ÀÌµ¿ ¼Óµµ:" + card.speed + "%";
+        childs.GetChild(3).GetComponent<TextMeshProUGUI>().text = "°ø°Ý ¼Óµµ:" + card.AttackSpeed + "%";
+        childs.GetChild(4).GetComponent<TextMeshProUGUI>().text = "Ã¼·Â:" +card.Health + "%";
+       
+
+        transform.GetChild(1).gameObject.SetActive(true);
+        _data = new CardData();
+        _data.Name = card.Name;
+        _data.Attack = card.Attack;
+        _data.speed = card.speed;
+        _data.AttackSpeed = card.AttackSpeed;
+        _data.Health = card.Health;
+        
+        SetText(_data.Name);
+        
+    }
+
+
+    private void SetText(string text)
+    {
+        transform.GetChild(1).GetComponent<TextMeshProUGUI>().DOText(text, 1f).SetUpdate(true);
     }
 }
--- Assets/DY/Managers/CardManager.cs
+++ Assets/DY/Managers/CardManager.cs
@@ -11,19 +11,17 @@ using Random = UnityEngine.Random;
 using UnityEngine.InputSystem.HID;
 using UnityEngine.Rendering;
 using System.Text.RegularExpressions;
+using UnityEngine.Serialization;
+
 public class CardManager : MonoSingleton<CardManager>
 {
 
-    string[] var = null;
     [SerializeField] private List<GameObject> _well = null;
     [SerializeField] private GameObject _CardPrefab = null;
-    private Transform child = null;
-    public float duration = 0.2f;
-    [SerializeField] private CardSO Sprite = null;
+    [FormerlySerializedAs("Sprite")] [SerializeField] private CardSO cardSo = null;
     [SerializeField] private AwakeSO[] RandomCardSO = null;
     public CardCheacker[] _cheack = null;
     [SerializeField] PlayerDataSO _playerDataSO = null;
-    GameObject Obiion;
     static int spriterand= 0;
     [SerializeField] private Transform EndTarget;
     [SerializeField] private Transform Target;
@@ -31,18 +29,16 @@ public class CardManager : MonoSingleton<CardManager>
     private CheckLevelUp _checkLevelUp;
     private int a = 0;
     public int stage = 0;
-public GameObject[] volumes;
+    public GameObject[] volumes;
     private void Awake()
     {
         _well = new List<GameObject>(2);
         _cheack = new CardCheacker[3];
-        child= transform.GetChild(0);
      
     }
 
     private void Start()
     {
-        
         for (int i = 0; i <= 1; i++)
         {
             volumes[i].GetComponent<Volume>().enabled = false;
@@ -81,7 +77,6 @@ public GameObject[] volumes;
         {
             _well.Add(Instantiate(_CardPrefab, transform));
             _cheack[i] = _well[i].transform.GetChild(0).GetComponent<CardCheacker>();
-            _cheack[i]._OnClick += RandomSprite;
             _well[i].GetComponentInParent<Canvas>().worldCamera = Camera.main;
             _well[i] = _well[i].transform.GetChild(0).gameObject;
         }
@@ -97,173 +92,69 @@ public GameObject[] volumes;
             Camera.main,
             out targetCanvasPosition
         );
-        Debug.Log("A");
         Time.timeScale = 0;
         // 기준 위치 (중앙 카드의 위치)
         float baseX = targetCanvasPosition.x;
 
         // 중앙 카드 배치
-        _well[1].GetComponent<RectTransform>().DOAnchorPos(new Vector2(baseX, targetCanvasPosition.y), duration).SetUpdate(true);
+        _well[1].GetComponent<RectTransform>().DOAnchorPos(new Vector2(baseX, targetCanvasPosition.y), 0.2f).SetUpdate(true);
 
         // 오른쪽 카드 배치
-        _well[2].GetComponent<RectTransform>().DOAnchorPos(new Vector2(baseX + fixedOffset, targetCanvasPosition.y), duration).SetUpdate(true);
+        _well[2].GetComponent<RectTransform>().DOAnchorPos(new Vector2(baseX + fixedOffset, targetCanvasPosition.y), 0.2f).SetUpdate(true);
 
         // 왼쪽 카드 배치
-        _well[0].GetComponent<RectTransform>().DOAnchorPos(new Vector2(baseX - fixedOffset, targetCanvasPosition.y), duration).SetUpdate(true).OnComplete(() =>
+        _well[0].GetComponent<RectTransform>().DOAnchorPos(new Vector2(baseX - fixedOffset, targetCanvasPosition.y), 0.2f).SetUpdate(true).OnComplete(() =>
         {
             // 애니메이션 완료 후 수행할 작업
         });
 
+        volumes[0].GetComponent<Volume>().enabled = true;
         // 카드 초기화
         BackSpriteFOr();
-        volumes[0].GetComponent<Volume>().enabled = true;
     }
 
     public void EndCardPolling()
-    { // �� ���Ⱑ ��������
+    { 
         Time.timeScale = 1;
         for (int i = 0; i < 3; i++)
-            _well[i].transform.DOMove(new Vector3(EndTarget.position.x, EndTarget.position.y - 75, EndTarget.position.z), duration).SetUpdate(true).OnComplete(() =>
+            _well[i].transform.DOMove(new Vector3(EndTarget.position.x, EndTarget.position.y - 75, EndTarget.position.z), 0.2f).SetUpdate(true).OnComplete(() =>
             {
                 for (int i = 0; i < 3; i++)
                     Destroy(_well[i].transform.parent.gameObject);
                 _well = null;
                 _cheack = null;
-                _cheack[i]._OnClick -= RandomSprite;
             });
 
         volumes[0].GetComponent<Volume>().enabled = false;
 
     }
     public void BackSpriteFOr()
-    {for (int i = 0; i < 3; i++)
-        {_well[i].transform.DOScaleX(0.18f, duration).SetUpdate(true);
-            _well[i].transform.GetChild(0).GetChild(0).gameObject.SetActive(false);
-            _well[i].transform.GetChild(0).GetComponent<Image>().sprite = Sprite.BackSprite;
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            _well[i].transform.DOScaleX(0.18f, 0.2f).SetUpdate(true);
             _well[i].transform.GetChild(0).GetChild(0).gameObject.SetActive(false);
         }   
     }
     public void FrontSprite<T>(T target) where T : Component
-    {child = target.transform.GetChild(0);
-        ChildChange(-2f,true,Sprite.FrontSprite);
-     
-    }
-    private void ChildChange(float end,bool flag,Sprite sprite)
     {
-        child.DOScaleX(end, duration).SetUpdate(true);
-        child.GetChild(0).gameObject.SetActive(flag);
-        child.GetComponent<Image>().sprite = sprite;
-        
-
     }
-
-    public void GameobjectGet(GameObject obj,bool flag)
-    {
-        try
-        {
-            obj.gameObject.SetActive(flag);
-            Obiion = obj;
-        }
-        catch (NullReferenceException)
-        {
-
-            Debug.LogError("null");
-            return;
-
-        } 
     
-    }
 
-    public void RandomSprite()
-    { 
-        if (stage == 0)
-            Stage0();
-        if (stage == 1)
-            Stage1();
-        if (stage == 2)
-            Stage2();
-    }
-    private void Stage2()
-    { 
-        int rand = Random.Range(0, 101);
-        if (rand <= 45)
-            EnterChange("Common");
-        if (rand > 45 && rand < 85)
-         EnterChange("Rare");
-        if (rand >= 85)
-         EnterChange("Legend");
-    }
-    private void Stage1()
-    {
-        int rand = Random.Range(0, 101);
-        if (rand <= 65)
-            EnterChange("Common");
-        if (rand is > 65 and < 90)
-            EnterChange("Rare");
-        if ( rand >= 90)
-            EnterChange("Legend");    
-    }
-    public void Stage0()
-    {int rand = Random.Range(0, 101);
-        if (rand <= 80)
-            EnterChange("Common");
-        
-        if (rand > 80)
-            EnterChange("Rare");
-    }
-    private void EnterChange<T>(T t)
-    {RandomCardSO = Resources.LoadAll<AwakeSO>($"RandomSprite/{t}");
-        spriterand = Random.Range(0, RandomCardSO.Length);
-        child.GetChild(0).GetComponent<Image>().sprite = RandomCardSO[spriterand].sprite;
-       
-        var = new string[]
-        {
-            RandomCardSO[spriterand].Name,
-            "공격력:" + RandomCardSO[spriterand].Attack  +"%",
-            "이동 속도:" + RandomCardSO[spriterand].speed + "%",
-            "공격 속도:" + RandomCardSO[spriterand].AttackSpeed + "%",
-             "체력:" +RandomCardSO[spriterand].Health + "%",
-        };
-        for (int i = 0; i < var.Length; i++)
-        { 
-            child.GetChild(0).GetChild(i).GetComponent<TextMeshProUGUI>().text = var[i];
-            
-          
-        }
-       
 
-        // 텍스트 파일로 저장
-     
 
-        //Debug.Log("Data saved to: " + path);
-        Obiion.gameObject.SetActive(true);
-        string text = RandomCardSO[spriterand].Text;
-       Ob(text);
-        
-  
-    }
 
-   private void Ob(string text)
-    {
-        Obiion.GetComponent<TextMeshProUGUI>().DOText(text, 1f).SetUpdate(true);
-    }
+
 
    
-    public void Enter(CardCheacker cardCheacker)
+    public void Enter(CardData cardData)
     {
 
-        Transform child = cardCheacker.transform.GetChild(0).GetChild(0);
-        for (int i = 0; i < 5; i++)
-        {
-            
-            Debug.Log(child.GetChild(i).GetComponent<TextMeshProUGUI>().text);
-        }
-
         PrintStat();
-        _playerDataSO.Damage += _playerDataSO.Damage*float.Parse(Regex.Match(child.GetChild(1).GetComponent<TextMeshProUGUI>().text, @"\d+").Value)/ 100;
-        _playerDataSO.Hp += _playerDataSO.Hp*float.Parse(Regex.Match(child.GetChild(2).GetComponent<TextMeshProUGUI>().text, @"\d+").Value)/ 100;
-        _playerDataSO.SwordAttackTime += _playerDataSO.SwordAttackTime*float.Parse(Regex.Match(child.GetChild(3).GetComponent<TextMeshProUGUI>().text, @"\d+").Value)/ 100;
-        _playerDataSO.MoveSpeed += _playerDataSO.MoveSpeed*float.Parse(Regex.Match(child.GetChild(4).GetComponent<TextMeshProUGUI>().text, @"\d+").Value) / 100;
+        _playerDataSO.Damage += _playerDataSO.Damage*(cardData.Attack/ 100);
+        _playerDataSO.Hp += _playerDataSO.Hp*cardData.Health/ 100;
+        _playerDataSO.SwordAttackTime += cardData.AttackSpeed/ 100;
+        _playerDataSO.MoveSpeed += cardData.speed / 100;
         PrintStat();
     }
 
